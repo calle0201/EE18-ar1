e7087bab9121aaeb1f1392b874695973d2f82088
calle <calle.edenflod@gmail.com>
2020-05-25 13:48:31 +0200

    mer bilder och en ring

--- javascript/mitt_slutprojekt/3dprint.js
+++ javascript/mitt_slutprojekt/3dprint.js
@@ -1,5 +1,5 @@
 //info about canvas
-const eCanvas = document.querySelector("#first_layer")
+const eCanvas = document.querySelector("#first_layer");
 eCanvas.width = 400;
 eCanvas.height = 400;
 
@@ -7,22 +7,33 @@ eCanvas.height = 400;
 var ctx = eCanvas.getContext("2d");
 
 //elements used / buttons
-const buttonCube = document.getElementById("cube")
-const buttonTriangle = document.getElementById("tringle")
+const buttonCube = document.getElementById("cube");
+const buttonTriangle = document.getElementById("tringle");
+const buttonCircle = document.getElementById("circle")
 
 //event listeners for buttons
 buttonCube.addEventListener("click", callCube)
 buttonTriangle.addEventListener("click", callTriangle)
-
+buttonCircle.addEventListener("click", callCircle)
 //varibles 
 var i = 0
 var interval;
 
 
+
+
+
+function callCircle() {
+    ctx.clearRect(0, 0, eCanvas.width*2, eCanvas.height*2);
+    ctx.beginPath();
+    clearInterval(interval);
+//start for first layer ish of a cube
+ interval =  setInterval(drawCircle, 40)
+}
 function callCube() {
     //clears earlier layers
     ctx.clearRect(0, 0, eCanvas.width*2, eCanvas.height*2);
-    ctx.beginPath()
+    ctx.beginPath();
     clearInterval(interval);
 //start for first layer ish of a cube
  interval =  setInterval(drawCube, 10)
@@ -34,7 +45,7 @@ function callTriangle() {
     ctx.beginPath()
     clearInterval(interval);
     //start for first layer ish of a cube
- interval =  setInterval(drawTriangle, 10)
+ interval =  setInterval(drawTriangle, 10);
 }
 
 
@@ -104,4 +115,24 @@ function drawCube() {
     i = 0
     clearInterval(interval);
 }
+}
+function drawCircle() {
+ i++
+      console.log(i);
+      
+    if (i <= 52) {
+        ctx.beginPath();
+        ctx.lineWidth = 10
+        //varför göra detta att det funkar gjorde mindre korade sätt och det funka inte
+        ctx.arc(200, 200, 100,0,i/8)
+        ctx.stroke()
+    
+        
+
+        //same youtube vid
+       
+    } else {
+        i = 0
+        clearInterval(interval);
+    }
 }
