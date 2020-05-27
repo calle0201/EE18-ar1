94afb9566ba9ef5bc615848483ec5f4dae339e4e
calle <calle.edenflod@gmail.com>
2020-05-27 12:19:00 +0200

    formaterade texten

--- javascript/mitt_slutprojekt/3dprint.js
+++ javascript/mitt_slutprojekt/3dprint.js
@@ -24,68 +24,70 @@ var R = 100
 
 
 function callCircle() {
-    ctx.clearRect(0, 0, eCanvas.width*2, eCanvas.height*2);
+    ctx.clearRect(0, 0, eCanvas.width * 2, eCanvas.height * 2);
     ctx.beginPath();
     clearInterval(interval);
-//start for first layer ish of a cube
- interval =  setInterval(drawCircle, 40)
+    //start for first layer ish of a cube
+    interval = setInterval(drawCircle, 40)
 }
+
 function callCube() {
     //clears earlier layers
-    ctx.clearRect(0, 0, eCanvas.width*2, eCanvas.height*2);
+    ctx.clearRect(0, 0, eCanvas.width * 2, eCanvas.height * 2);
     ctx.beginPath();
     clearInterval(interval);
-//start for first layer ish of a cube
- interval =  setInterval(drawCube, 10)
+    //start for first layer ish of a cube
+    interval = setInterval(drawCube, 10)
 }
+
 function callTriangle() {
     //clears earlier layers
-    ctx.clearRect(0, 0, eCanvas.width*2, eCanvas.height*2);
-   
+    ctx.clearRect(0, 0, eCanvas.width * 2, eCanvas.height * 2);
+
     ctx.beginPath()
     clearInterval(interval);
     //start for first layer ish of a cube
- interval =  setInterval(drawTriangle, 10);
+    interval = setInterval(drawTriangle, 10);
 }
 
 
 
 function drawTriangle() {
-   //start the layer of a triangle
-   if (i < 10) {
-       i = 199
-   }
+    //start the layer of a triangle
+    if (i < 10) {
+        i = 199
+    }
     if (i == 200) {
         ctx.beginPath();
         ctx.moveTo(200, 200)
         ctx.lineWidth = 10
     }
     i++
-     if (i <= 300 && i > 200) {
-     ctx.lineTo(i ,i )
-ctx.stroke()
+    if (i <= 300 && i > 200) {
+        ctx.lineTo(i, i)
+        ctx.stroke()
     } else if (i <= 450) {
-        
-        ctx.lineTo(-i +600 ,300 )
+
+        ctx.lineTo(-i + 600, 300)
         ctx.stroke()
     } else if (i <= 460) {
-        
-        ctx.lineTo( 200 , 200)
+
+        ctx.lineTo(200, 200)
         ctx.stroke()
-}else if (i <= 461) {
-    
-    ctx.lineTo(300,300)
-    ctx.lineTo(200,300)
-    ctx.fill()
-   
-} else if (i == 501) {
-    i = 0
-    clearInterval(interval);
-}
+    } else if (i <= 461) {
+
+        ctx.lineTo(300, 300)
+        ctx.lineTo(200, 300)
+        ctx.fill()
+
+    } else if (i == 501) {
+        i = 0
+        clearInterval(interval);
+    }
 }
 
 function drawCube() {
-//the first layer of a cube
+    //the first layer of a cube
     if (i == 1) {
         ctx.beginPath();
         ctx.moveTo(10, 10)
@@ -105,39 +107,35 @@ function drawCube() {
         ctx.fillRect(10, 206, 20, -i + 590)
         ctx.fill()
     } else if (i <= 950) {
-        ctx.fillRect(i -770, 10, 10, i -750)
+        ctx.fillRect(i - 770, 10, 10, i - 750)
         ctx.fill()
     } else if (i <= 1120) {
-    ctx.fillRect(200, 200, -i+950 , -i+ 950)
-    ctx.fill()
-    
-} else if (i == 1121) {
-    i = 0
-    clearInterval(interval);
+        ctx.fillRect(200, 200, -i + 950, -i + 950)
+        ctx.fill()
+
+    } else if (i == 1121) {
+        i = 0
+        clearInterval(interval);
+    }
 }
-}
+
 function drawCircle() {
- i++
-     
-      
+    i++
+
+
     if (i <= 52) {
         ctx.beginPath();
         ctx.lineWidth = 10
         //varför göra detta att det funkar gjorde mindre korade sätt och det funka inte
-        ctx.arc(200, 200, R,0,i/8)
+        ctx.arc(200, 200, R, 0, i / 8)
         ctx.stroke()
-    
-        
-
-        //same youtube vid
-       
+        //same youtube vid  
     } else {
         i = 0
         R -= 10
         console.log(R);
         console.log(i);
-        
-        
+
     }
     if (R < 0) {
         i = 0
